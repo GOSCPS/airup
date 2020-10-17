e72e1aa3487687290c078b7ad8a610a862ce82ce
sisungo <otelve@outlook.com>
2020-10-18 04:26:32 +0800

    airupd added

---
+++ airupd/src/main.rs
@@ -0,0 +1,178 @@
+#[macro_use]
+extern crate ini;
+
+use ansi_term::Color::*;
+use libc::{sigfillset, sigprocmask, sigset_t, SIG_BLOCK};
+use regex::Regex;
+use std::collections::HashMap;
+use std::env;
+use std::mem;
+use std::process::exit;
+use std::path::Path;
+use std::process::Command;
+
+static VERSION: &str = "0.1";
+#[cfg(not(feature = "airupdbg"))]
+static AIRUP_CFG: &str = "/etc/airup.conf";
+#[cfg(feature = "airupdbg")]
+static AIRUP_CFG: &str = "./test/airup.conf";
+fn get_target_dir(ad:&str, name:&str) -> String {
+    let mut rslt = ad.to_string();
+	if !ad.ends_with("/") {
+		rslt.push('/');
+	}
+	rslt.push_str(name);
+	rslt.push('/');
+	rslt
+}
+fn target_exec(td:&str) {
+	let dir = td.to_string();
+	println!("{}Switching target to {}...", Blue.paint(" * "), Green.paint(td.clone()));
+	if !Path::new(td.clone()).exists() {
+		println!("{}Failed to switch target to {}: file does not exist!", Red.paint(" * "), Green.paint(td.clone()));
+        return;
+	}
+    let mut f = dir.clone();
+    f.push_str("target.conf");
+    let z = &f;
+    let i = ini!(safe z);
+    let i = match i {
+    	Ok(a) => a,
+    	Err(b) => {
+    		println!("{}Target main configuration parse error(path: {})", Red.paint(" * "), f);
+    		eprintln!("{}{}", Red.paint(" * "), b);
+    		return;
+    	},
+    };
+    let name = tget(&i, "name");
+    match name {
+    	Some(a) => println!("{}Switched target to {}!", Green.paint(" * "), Green.paint(a)),
+    	None => println!("{}Switched target to {}!", Green.paint(" * "), Green.paint(&dir)),
+    };
+}
+fn resolve_args(argv: &Vec<String>) -> String {
+    let mut c: String = String::from("default");
+    if argv.len() == 1 {
+        return "default".to_string();
+    }
+    for i in argv.iter() {
+        if i == "single" {
+            return "single".to_string();
+        } else {
+            let re = Regex::new(r"(?x)target=(?P<t>\w*)").unwrap();
+            if i.to_string() == argv[0] {
+                continue;
+            } else {
+                let caps = re.captures(i);
+                c = match caps {
+                    Some(b) => b["t"].to_string(),
+                    None => "default".to_string(),
+                };
+            }
+        }
+    }
+    c
+}
+fn print_svc_prompt(name: &str, desc: &str) {
+    println!(
+        "{}Starting {}({})...",
+        Green.paint(" * "),
+        Green.paint(name),
+        Blue.paint(desc)
+    );
+}
+fn sexec_monitor(s: &HashMap<String, HashMap<String, Option<String>>>) {}
+fn tget(s: &HashMap<String, HashMap<String, Option<String>>>, c: &str) -> Option<String> {
+	if s.get("target") == None {
+		return None;
+	}
+	match &s["target"].get(c.clone()).clone() {
+		Some(a) => Some(a.as_ref().unwrap().to_string()),
+		None => None,
+	}
+}
+fn sget(s: &HashMap<String, HashMap<String, Option<String>>>, c: &str) -> Option<String> {
+    if s.get("svc") == None {
+        return None;
+    }
+    match &s["svc"].get(c.clone()).clone() {
+        Some(a) => Some(a.as_ref().unwrap().to_string()),
+        None => None,
+    }
+}
+fn cget(s: &HashMap<String, HashMap<String, Option<String>>>, c: &str) -> Option<String> {
+    if s.get("rc") == None {
+        return cget_default(c.clone());
+    }
+    match &s["rc"].get(c.clone()).clone() {
+        Some(a) => Some(a.as_ref().unwrap().to_string()),
+        None => cget_default(c.clone()),
+    }
+}
+fn cget_default(c: &str) -> Option<String> {
+    if c == "airup_dir" {
+        Some(String::from("/etc/airup.d"))
+    } else if c == "distro" {
+        Some(String::from("Unknown UNIX"))
+    } else {
+        None
+    }
+}
+fn main() {
+    #[cfg(not(feature = "airupdbg"))]
+    {
+        if nix::unistd::getpid().as_raw() != 1 {
+            println!("{}This program only runs as PID 1.", Red.paint(" * "));
+            println!(
+                "{}If you are trying to control Airup, use 'airupctl'.",
+                Red.paint(" * ")
+            );
+            exit(-1);
+        }
+    }
+    unsafe {
+        let mut s1: sigset_t = mem::zeroed();
+        let mut s2: sigset_t = mem::zeroed();
+        sigfillset(&mut s1 as *mut sigset_t);
+        sigprocmask(
+            SIG_BLOCK,
+            &mut s1 as *mut sigset_t,
+            &mut s2 as *mut sigset_t,
+        );
+    }
+    let cfgset = ini!(safe AIRUP_CFG);
+    let cfgset = match cfgset {
+        Ok(a) => a,
+        Err(_b) => {
+            println!(
+                "{}: airup main config file not found!!!",
+                Red.paint("error")
+            );
+            println!("Now airup will spawn /bin/sh. You can try to fix. Good luck!");
+            let sh = Command::new("/bin/sh").spawn();
+            match sh {
+                Ok(mut a) => a.wait().unwrap(),
+                Err(_b) => {
+                    println!("\nFailed to spawn /bin/sh.");
+                    loop {}
+                }
+            };
+            main();
+            exit(0);
+        }
+    };
+    let airup_dir = cget(&cfgset, "airup_dir").unwrap();
+    #[cfg(feature = "airupdbg")]
+    {
+        println!("Airup Directory: {}", &airup_dir);
+    }
+    println!(
+        "{} {} is starting {}...",
+        Blue.paint("Airup"),
+        VERSION,
+        Green.paint(cget(&cfgset, "distro").unwrap())
+    );
+    let argv: Vec<String> = env::args().collect();
+    let target = resolve_args(&argv);
+    target_exec(&get_target_dir(&airup_dir, &target));
+}
