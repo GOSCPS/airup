c89e84c0e7a7a7c5fd1b46e8c89d651bca72dd3f
sisungo <otelve@outlook.com>
2020-11-15 16:49:35 +0800

    BUG ADD: Unable to pass build cause of recursive async fn about Milestone dependency resolving. FEATURE ADD: Milestones are basically done.

--- airupd/src/main.rs
+++ airupd/src/main.rs
@@ -1,5 +1,5 @@
 use ansi_term::Color::*;
-use async_std::{fs, prelude::*, println, sync::RwLock, task, io, path::PathBuf};
+use async_std::{fs, prelude::*, println, sync::RwLock, task, io, path::{PathBuf, Path}};
 use libc::{getpid, pid_t, sigfillset, sigprocmask, sigset_t, uid_t, SIG_BLOCK};
 use once_cell::sync::Lazy;
 use std::{
@@ -11,7 +11,7 @@ use std::{
     process::exit,
     process::{Child, Command},
 };
-use toml::Value;
+use toml::{Value, map::Map};
 
 enum User {
     Id(uid_t),
@@ -40,7 +40,14 @@ enum SvcStatus {
     Stopped,
     Unmentioned,
 }
+enum Stage {
+	PreStart,
+	Milestones(String),
+	Shutdown,
+	CtrlAltDel,
+}
 
+static CURRENT_STAGE: Lazy<RwLock<Stage>> = Lazy::new(|| RwLock::new(Stage::PreStart));
 static AIRUP_VERSION: &str = env!("CARGO_PKG_VERSION");
 static SVC_STATUS: Lazy<RwLock<HashMap<String, SvcStatus>>> =
     Lazy::new(|| RwLock::new(HashMap::new()));
@@ -65,6 +72,22 @@ static DEFAULT_CFG: Lazy<HashMap<String, Value>> = Lazy::new(|| {
         "internal/prestart_dir".to_string(),
         Value::String(default_prestart_dir),
     );
+    a.insert(
+    	"milestone/description".to_string(),
+    	Value::String("An airup milestone".to_string())
+    );
+    a.insert(
+    	"milestone/paral".to_string(),
+    	Value::Boolean(true)
+    );
+    a.insert(
+    	"milestone/env_list".to_string(),
+    	Value::Table(Map::new())
+    );
+    a.insert(
+    	"milestone/dependencies".to_string(),
+    	Value::Array(vec![])
+    );
     a
 });
 
@@ -75,7 +98,7 @@ static AIRUP_CONF: &str = "/etc/airup.conf";
 static AIRUP_CONF: &str = "debug/airup.conf";
 
 async fn stage_prestart_exec(dir: &str, paral: bool) {
-    if !std::path::Path::new(dir.clone()).exists() {
+    if !Path::new(dir.clone()).exists().await {
         println!(
             "{}The specified prestart stage directory {} does not exist. Aborting...",
             Red.paint(" * "),
@@ -104,6 +127,10 @@ async fn stage_prestart_exec(dir: &str, paral: bool) {
     	rda.push(i.as_ref().unwrap().path());
     }
     let rd = rda;
+    println!(
+    	"{}Running PreStart Objects...",
+    	Green.paint(" * ")
+    ).await;
     for i in rd.iter() {
         let child = system(&i.to_string_lossy()).await;
         if child.is_none() {
@@ -211,19 +238,29 @@ fn tomlget(set: Option<Value>, ns: &str, vid: &str) -> Option<Value> {
     }
     Some(rslt.unwrap().clone())
 }
-fn g_airupconf(set: Option<Value>, vid: &str) -> Option<Value> {
+fn g_airupconf(set: Option<Value>, vid: &str) -> Value {
     let temp = tomlget(set, "airup", vid.clone());
+    let default = Lazy::new(|| get_default_value("airup", vid).unwrap());
     if temp.is_none() {
-        return get_default_value("airup", vid);
+        return default.clone();
     }
     let temp = temp.unwrap();
     if (!temp.is_str()) && (vid == "osname" || vid == "airup_home" || vid == "env_path") {
-        return get_default_value("airup", vid);
+        return default.clone();
     } else if (!temp.is_bool()) && vid == "prestart_paral" {
-        return get_default_value("airup", vid);
+        return default.clone();
     }
     let rslt = temp;
-    Some(rslt)
+    rslt
+}
+fn g_milestonetoml(set: Option<Value>, vid: &str) -> Option<Value> {
+	let temp = tomlget(set, "milestone", vid.clone());
+	let default = Lazy::new(|| get_default_value("milestone", vid));
+	if temp.is_none() && vid != "prompt" && vid != "pre_exec" {
+		return Some(default.as_ref().unwrap().clone());
+	}
+	let rslt = temp;
+	rslt
 }
 async fn pid_detect() {
     #[cfg(not(feature = "quickdbg"))]
@@ -263,7 +300,96 @@ fn set_airenv(ms: &str, ad: &str, par: bool) {
 fn set_panic() {
     panic::set_hook(Box::new(|panic_info| {
         std::println!("[!!!]Airup Panic");
+        std::eprintln!("Debug Data: {}", panic_info);
     }));
+}
+// The following two functions are made for "dependencies" toml object resolving.
+fn str_to_vec(s: &str) -> Vec<String> {
+	vec![s.to_string()]
+}
+async fn vv_to_vs(vv: Vec<Value>) -> Vec<String> {
+    let mut vs = Vec::new();
+	for i in vv {
+		if !i.is_str() {
+			continue;
+		}
+		let i = i.as_str().unwrap();
+		vs.push(i.to_string());
+	}
+	vs
+}
+async fn toml_env_setup(env_list: Map<String, Value>) {
+	let keys: Vec<&String> = env_list.keys().collect();
+	for key in keys {
+		let value = env_list.get(key).unwrap();
+		if !value.is_str() {
+			continue;
+		}
+		let value = value.as_str().unwrap();
+		env::set_var(key, value);
+	}
+}
+async fn milestone_dep(mdir: &str, deps: Vec<String>) {
+	for i in deps.iter() {
+		let mut dir = PathBuf::from(mdir.clone());
+		dir.push(i);
+		milestone_exec(&dir.to_string_lossy()).await;
+	}
+}
+async fn milestone_exec(dir: &str) {
+    // Judge if the milestone exists
+    if !Path::new(dir.clone()).exists().await {
+    	println!("{}The specified milestone {} does not exist.", Red.paint(" * "), Red.paint(dir.clone())).await;
+    	return;
+    }
+    // Find milestone.toml
+    let mut mtpath = PathBuf::from(dir.clone());
+    mtpath.push("milestone.toml");
+    // Ready data
+	let milestone_toml = get_toml_of(&mtpath.to_string_lossy()).await;
+	let invalid = std::ffi::OsString::from("invalid");
+	let default_prompt = mtpath.file_name().unwrap_or(&invalid).to_string_lossy();
+	let prompt = g_milestonetoml(milestone_toml.clone(), "prompt").unwrap_or(Value::String(default_prompt.to_string()));
+	let prompt = prompt.as_str().unwrap();
+	let description = g_milestonetoml(milestone_toml.clone(), "description").unwrap();
+	let description = description.as_str().unwrap();
+	let paral = g_milestonetoml(milestone_toml.clone(), "paral").unwrap().as_bool().unwrap();
+	let env = g_milestonetoml(milestone_toml.clone(), "env_list").unwrap();
+	let env = env.as_table().unwrap();
+	// pre_exec may be Option::None.
+	let pre_exec = g_milestonetoml(milestone_toml.clone(), "pre_exec");
+	let dependencies = g_milestonetoml(milestone_toml, "dependencies").unwrap();
+	let dependencies = dependencies.as_array().unwrap();
+	let _files = fs::read_dir(dir.clone()).await;
+	if _files.is_err() {
+		println!("{}The specified milestone path {} is not a directory.", Red.paint(" * "), Red.paint(dir.clone())).await;
+		return;
+	}
+	let _files: Vec<io::Result<fs::DirEntry>> = _files.unwrap().collect().await;
+	let mut files: Vec<PathBuf> = Vec::new();
+	for i in _files.iter() {
+		files.push(i.as_ref().unwrap().path());
+	}
+	// Action
+	toml_env_setup(env.clone()).await;
+	println!("{}Reaching milestone {}({})...", Green.paint(" * "), Green.paint(prompt.clone()), Purple.paint(description)).await;
+    (*CURRENT_STAGE.write().await) = Stage::Milestones(prompt.to_string());
+    match pre_exec {
+    	Some(a) => {
+    		if !a.is_str() {
+    			()
+    		}
+    		let a = a.as_str().unwrap();
+    		system(a).await;
+    	},
+    	None => ()
+    };
+    milestone_dep(&Path::new(dir.clone()).parent().unwrap().to_string_lossy(), vv_to_vs(dependencies.clone()).await).await;
+}
+async fn stage_milestone_start(dir: &str, milestone: &str) {
+	let mut dir = PathBuf::from(dir);
+	dir.push(milestone);
+	milestone_exec(&dir.to_string_lossy()).await;
 }
 #[async_std::main]
 async fn main() {
@@ -271,11 +397,10 @@ async fn main() {
     set_panic();
     let airup_conf = get_toml_of(AIRUP_CONF).await;
     //Prepare some values from airup.conf
-    let osname = g_airupconf(airup_conf.clone(), "osname").unwrap();
+    let osname = g_airupconf(airup_conf.clone(), "osname");
     let osname = osname.as_str().unwrap();
     pathsetup(
         g_airupconf(airup_conf.clone(), "env_path")
-            .unwrap()
             .as_str()
             .unwrap(),
     );
@@ -287,9 +412,9 @@ async fn main() {
     )
     .await;
     let milestone = get_milestone();
-    let airup_home = g_airupconf(airup_conf.clone(), "airup_home").unwrap();
+    let airup_home = g_airupconf(airup_conf.clone(), "airup_home");
     let airup_home = airup_home.as_str().unwrap();
-    let prestart_paral = g_airupconf(airup_conf, "prestart_paral").unwrap();
+    let prestart_paral = g_airupconf(airup_conf, "prestart_paral");
     let prestart_paral = prestart_paral.as_bool().unwrap();
     set_airenv(&milestone, airup_home.clone(), prestart_paral);
     let mut prestart_dir = PathBuf::from(airup_home.clone());
@@ -304,4 +429,8 @@ async fn main() {
         prestart_paral,
     )
     .await;
+    let mut milestones_dir = PathBuf::from(airup_home.clone());
+    milestones_dir.push("milestones");
+    let milestones_dir = milestones_dir.to_string_lossy();
+    stage_milestone_start(&milestones_dir, &milestone).await;
 }
